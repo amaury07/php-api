4ef596814310a1b0b99d2a57d9c368a2349019eb
Amaury KHELIFI <amaury@khelifi.org>
2014-07-14 01:37:59 +0200

    subscription by Facebook

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new SpiceMe\ApiBundle\SpiceMeApiBundle(),
+            new SpiceMe\DomainBundle\SpiceMeDomainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
